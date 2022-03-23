abf9522e5879c7983e79e5f2c82dc2973acdadd2
Georgia Witchel <gwitchel@gmail.com>
2022-03-22 20:48:36 -0700

    changed priority buttons

--- src/Priority.js
+++ src/Priority.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import LooksOneTwoToneIcon from '@mui/icons-material/LooksOneTwoTone';
-import LooksTwoTwoToneIcon from '@mui/icons-material/LooksTwoTwoTone';
-import Looks3TwoToneIcon from '@mui/icons-material/Looks3TwoTone';
+import Icon from "@mdi/react";
+import {mdiChevronUp,mdiChevronDoubleUp, mdiChevronTripleUp} from '@mdi/js';
+
+
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
 
 // priority button receives priority through props and returns the correct button
 export default function Priority(props) {
-    const PriorityIcons = [<Looks3TwoToneIcon/>,<LooksTwoTwoToneIcon/>,<LooksOneTwoToneIcon/>]
+
+    const PriorityIcons = [
+        
+        <Icon path={mdiChevronTripleUp} title="urgent" size={1} />,
+        <Icon path={mdiChevronDoubleUp} title="normal" size={1}  />,
+        <Icon path={mdiChevronUp} title = "casual" size={1} /> ]
+
     return (
-        <div style={{opacity:0.75}}>
-         {PriorityIcons[props.priority-1]}
-        </div>
+        PriorityIcons[props.priority-1]
     )
 }
