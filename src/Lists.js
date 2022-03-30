4f4019d61945b70ee3d919115f0a7a434f9efc0e
Qing-Yang-1211 <zzzyangqing@gmail.com>
2022-03-30 15:31:30 -0700

    try deploy

--- src/Lists.js
+++ src/Lists.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import {useState} from 'react';
-import {generateUniqueID} from "web-vitals/dist/modules/lib/generateUniqueID";
+// import {generateUniqueID} from "web-vitals/dist/modules/lib/generateUniqueID";
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from "firebase/firestore";
-import { serverTimestamp } from "firebase/firestore";
-import { setDoc, doc } from "firebase/firestore";
+// import { serverTimestamp } from "firebase/firestore";
+// import { setDoc, doc } from "firebase/firestore";
 import { query, collection } from "firebase/firestore";
 import { useCollectionData } from "react-firebase-hooks/firestore";
 import Tab from '@mui/material/Tab';
 import Tabs from '@mui/material/Tabs';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
-import Typography from '@mui/material/Typography';
+// import TextField from '@mui/material/TextField';
+// import Dialog from '@mui/material/Dialog';
+// import DialogActions from '@mui/material/DialogActions';
+// import DialogContent from '@mui/material/DialogContent';
+// import DialogContentText from '@mui/material/DialogContentText';
+// import DialogTitle from '@mui/material/DialogTitle';
+// import Typography from '@mui/material/Typography';
 import List from './List';
 
 // web app's Firebase configuration
@@ -64,51 +64,51 @@ function ListTabs(props) {
 export default function Lists(props) {
     const q = query(collection(db, 'Lists'));   
     const [lists, loading, error] = useCollectionData(q);
-    const [newListDialogOpen, setNewListDialogOpen] = React.useState(false);
-    const [listName, setListName] = useState("");
-    const [showAlert, setShowAlert] = React.useState(false);
+    // const [newListDialogOpen, setNewListDialogOpen] = React.useState(false);
+    // const [listName, setListName] = useState("");
+    // const [showAlert, setShowAlert] = React.useState(false);
 
-    const handleNewListDialogOpen = () => {
-        setNewListDialogOpen(true);
-    };
+    // const handleNewListDialogOpen = () => {
+    //     setNewListDialogOpen(true);
+    // };
 
-    const handleNewListDialogClose = () => {
-        setNewListDialogOpen(false);
-        setShowAlert(false);
-        setListName("")
-    };
+    // const handleNewListDialogClose = () => {
+    //     setNewListDialogOpen(false);
+    //     setShowAlert(false);
+    //     setListName("")
+    // };
 
-    const handleSetListName = e => {
-        setListName(e.target.value)
-        if (e.target.value.length>0) {
-            setShowAlert(false);
-        }else{
-            setShowAlert(true);
-        }
-    }
+    // const handleSetListName = e => {
+    //     setListName(e.target.value)
+    //     if (e.target.value.length>0) {
+    //         setShowAlert(false);
+    //     }else{
+    //         setShowAlert(true);
+    //     }
+    // }
 
-    function handleSubmitNewList(){
-        const listId = generateUniqueID()
-        setDoc(doc(db, "Lists", listId), {
-            id: listId,
-            name: listName,
-            showCompleted: false,
-            sortBy: "priority",
-            sortDir: "asc",
-            created: serverTimestamp(),
-        })
-    }
+    // function handleSubmitNewList(){
+    //     const listId = generateUniqueID()
+    //     setDoc(doc(db, "Lists", listId), {
+    //         id: listId,
+    //         name: listName,
+    //         showCompleted: false,
+    //         sortBy: "priority",
+    //         sortDir: "asc",
+    //         created: serverTimestamp(),
+    //     })
+    // }
 
-    function onSubmitNewList(e) {
-        if (listName.length===0) {
-            setShowAlert(true)
-            setListName("")
-        }else{
-            handleSubmitNewList()
-            handleNewListDialogClose();
-            setListName("")
-        }
-    }
+    // function onSubmitNewList(e) {
+    //     if (listName.length===0) {
+    //         setShowAlert(true)
+    //         setListName("")
+    //     }else{
+    //         // handleSubmitNewList()
+    //         handleNewListDialogClose();
+    //         setListName("")
+    //     }
+    // }
     
     if (loading) {
         return (
@@ -118,11 +118,12 @@ export default function Lists(props) {
         return (
         <>
             <Box sx={{display:'flex', flexDirection: 'row', alignItems:'center', justifyContent:'flex-start'}}>
-                <Button onClick={handleNewListDialogOpen} variant='outlined' sx={{textTransform:'none', marginRight:2}}>Start a New List</Button>
+                {/* <Button onClick={handleNewListDialogOpen} variant='outlined' sx={{textTransform:'none', marginRight:2}}>Start a New List</Button> */}
+                <Button variant='outlined' sx={{textTransform:'none', marginRight:2}}>Start a New List</Button>
                 <Button variant='outlined' sx={{textTransform:'none'}}>Delete Current List</Button>
             </Box>
             <ListTabs lists={lists}/>
-            <Dialog open={newListDialogOpen} onClose={handleNewListDialogClose}>
+            {/* <Dialog open={newListDialogOpen} onClose={handleNewListDialogClose}>
                 <DialogTitle>Create a New List</DialogTitle>
                 <DialogContent>
                 <DialogContentText>
@@ -145,7 +146,7 @@ export default function Lists(props) {
                 <Button onClick={handleNewListDialogClose}>Cancel</Button>
                 <Button onClick={onSubmitNewList}>Submit</Button>
                 </DialogActions>
-            </Dialog>
+            </Dialog> */}
         </>)
     }
 }
